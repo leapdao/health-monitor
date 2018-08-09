e144989b308b234a6fc0b26f2bb2941bb1d98bd1
Alex Lunyov <isuntc@gmail.com>
2018-08-09 14:09:19 +0800

    Real alice url

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,14 @@ export default () => {
   return (
     <div className="page">
       <h1>PARSEC nodes health monitor</h1>
-      <Monitor nodes={[{ url: 'http://localhost:8646', label: 'Alice' }]} />
+      <Monitor
+        nodes={[
+          {
+            url: 'http://ec2-34-254-247-1.eu-west-1.compute.amazonaws.com:8545',
+            label: 'Alice',
+          },
+        ]}
+      />
     </div>
   );
 };
