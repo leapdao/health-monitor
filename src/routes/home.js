fedbc4b7dd8fbffc8265f198dcfcc86b1cfe49ee
Kosta Korenkov <kosta@whoot.me>
2018-08-22 00:14:10 +0300

    add terraformed node

--- src/routes/home.js
+++ src/routes/home.js
@@ -15,6 +15,10 @@ export default () => {
             url: 'https://testnet-2.parseclabs.org',
             label: 'Testnet #2',
           },
+          {
+            url: 'http://54.72.59.173:8645',
+            label: 'Terraformed node',
+          },
         ]}
       />
     </div>
